54516986e1d41062f89cd6d48adb21ffda80e5ea
Avenash Kumar <avenash_kumar@hotmail.com>
2021-08-08 22:37:33 -0400

    Update context path to avoid any future conflicts between api endpoints

--- frontend-market-stare-pwa/assets/scripts/javascript.js
+++ frontend-market-stare-pwa/assets/scripts/javascript.js
@@ -1,5 +1,5 @@
 //const g_host = "http://localhost:8081";
-const g_host = "https://gateway-marketstare-lnh3luj.uc.gateway.dev";
+const g_host = "https://gateway-marketstare-lnh3luj.uc.gateway.dev/backend-marketstare-search";
 
 const g_apiOverview = "/api/v1/overview/find/symbol/";
 
